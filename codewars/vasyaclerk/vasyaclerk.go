04948cd03f445606694e2c5bfadb23c0d5d0fadc
renjie.zhang <joker_zhang@aliyun.com>
2020-01-07 12:01:58 +0000

    add vasyavlerk 💰

--- codewars/vasyaclerk/vasyaclerk.go
+++ codewars/vasyaclerk/vasyaclerk.go
@@ -14,9 +14,40 @@ Tickets([]int{25, 100}) // => NO. Vasya will not have enough money to give chang
 Tickets([]int{25, 25, 50, 50, 100}) // => NO. Vasya will not have the right bills to give 75 dollars of change (you can't make two bills of 25 from one of 50)
 
 链接：https://www.codewars.com/kata/vasya-clerk/train/go
+/我的第一种做法是使用一个切片维护收到的钱，虽然很好解决在找零钱补50，但是到补零钱100就犯难了。
+在看了别人的题解之后，尝试使用map去维护收到的零钱。
 */
 package vasyaclerk
 
-func solution() {
-
+//Tickets ...
+func Tickets(peopleInLine []int) string {
+	var money = map[int]int{
+		25:  0,
+		50:  0,
+		100: 0,
+	}
+	for _, v := range peopleInLine {
+		switch true {
+		case v == 25:
+			money[25]++
+		case v == 50:
+			if money[25] == 0 {
+				return "NO"
+			} else {
+				money[25]--
+				money[50]++
+			}
+		case v == 100:
+			if money[50] == 1 && money[25] == 1 {
+				continue
+			} else if money[25] == 3 {
+				continue
+			} else {
+				return "NO"
+			}
+		default:
+			return "NO"
+		}
+	}
+	return "YES"
 }
