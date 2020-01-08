f72e9bd1e49e5ebf71bed9f04ccc522b771eef34
renjie.zhang <joker_zhang@aliyun.com>
2020-01-08 02:52:11 +0000

    update content🎈

--- LeetCode/climbingstairs/climbstairs.go
+++ LeetCode/climbingstairs/climbstairs.go
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2019-12-26 15:06:56
- * @LastEditTime: 2019-12-26 17:46:30
- * @LastEditors: your name
- * @Description: In User Settings Edit
- * @FilePath: \GoPath\src\Practice-Algorithmic\LeetCode\climbingstairs\climbstairs.go
- */
 package climbingstairs
 
 func climbStairs(n int) int {
