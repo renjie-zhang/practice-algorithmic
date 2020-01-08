73af3ca535c2f81aa37234b990717304df2305d6
renjie.zhang <joker_zhang@aliyun.com>
2020-01-08 03:26:02 +0000

    add removenode😁

--- LeetCode/removenthnodefromendoflist/removenode.go
+++ LeetCode/removenthnodefromendoflist/removenode.go
@@ -5,10 +5,24 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func main() {
-
-}
-
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-
+	var length = 1
+	temp := head
+	for temp.Next != nil {
+		length++
+		temp = temp.Next
+	}
+	if n < 0 || n > length {
+		return nil
+	}
+	if n == length {
+        head = head.Next
+		return head
+	}
+	current := head
+	for i := length - n - 1; i > 0; i-- {
+		current = current.Next
+	}
+	current.Next = current.Next.Next
+	return head
 }
