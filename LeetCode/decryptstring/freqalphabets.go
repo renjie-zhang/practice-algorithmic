ee321070712086fd4d5aade276dde97146640d5a
renjie.zhang <joker_zhang@aliyun.com>
2020-01-08 02:52:46 +0000

    add leetcode freqalphabets🥟

---
+++ LeetCode/decryptstring/freqalphabets.go
@@ -0,0 +1,19 @@
+package decryptstring
+
+/**
+题目链接：https://leetcode-cn.com/problems/decrypt-string-from-alphabet-to-integer-mapping/
+*/
+
+func freqAlphabets(s string) string {
+    var answer string
+	length := len(s)
+	for i := 0; i < length; i++ {
+		if i+2 < length && s[i+2] == '#' {
+			answer += string((rune(s[i])-'0')*10 + (rune(s[i+1]) - '1' + 'a'))
+			i += 2
+		} else {
+			answer += string(s[i] - '1' + 'a')
+		}
+	}
+	return answer
+}
