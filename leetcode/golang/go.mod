e430395129cdf792afecb15484455b3edf9a78ec
renjie zhang <joker_zhang@aliyun.com>
2021-08-12 22:35:18 +0800

    add maxSubArray

--- leetcode/golang/go.mod
+++ leetcode/golang/go.mod
@@ -1,1 +1,5 @@
-module "golang"
+module golang
+
+go 1.16
+
+require github.com/stretchr/testify v1.7.0
--- leetcode/golang/maxSubarray/subarray.go
+++ leetcode/golang/maxSubarray/subarray.go
@@ -14,11 +14,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package maxSubarray
 
-package hello;
+func MaxSubArray(nums []int) int {
+	max := nums[0]
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("AA");
-    }
+	for i := 1; i < len(nums); i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
 }
