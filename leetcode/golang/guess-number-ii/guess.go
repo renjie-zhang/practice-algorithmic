00f4efe44c6fb766c70dc1fd103fd664c7c7092c
renjie zhang <joker_zhang@aliyun.com>
2021-11-12 23:55:40 +0800

    Create guess.go

---
+++ leetcode/golang/guess-number-ii/guess.go
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package guess_number_ii
+
+import "math"
+
+func getMoneyAmount(n int) int {
+	f := make([][]int, n+1)
+	for i := range f {
+		f[i] = make([]int, n+1)
+	}
+	for i := n - 1; i >= 1; i-- {
+		for j := i + 1; j <= n; j++ {
+			minCost := math.MaxInt32
+			for k := i; k < j; k++ {
+				cost := k + max(f[i][k-1], f[k+1][j])
+				if cost < minCost {
+					minCost = cost
+				}
+			}
+			f[i][j] = minCost
+		}
+	}
+	return f[1][n]
+}
+
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
+}
