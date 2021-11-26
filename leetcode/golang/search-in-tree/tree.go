5c27a5cc4fb6e991c63bda5916fa2fea056ef576
renjie zhang <joker_zhang@aliyun.com>
2021-11-26 22:41:35 +0800

    Create tree.go

---
+++ leetcode/golang/search-in-tree/tree.go
@@ -0,0 +1,20 @@
+package search_in_tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if val == root.Val {
+		return root
+	}
+	if val < root.Val {
+		return searchBST(root.Left, val)
+	}
+	return searchBST(root.Right, val)
+}
